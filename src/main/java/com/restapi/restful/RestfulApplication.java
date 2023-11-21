87c42bd429e56ad470f3cbd9f1fea9b71dfc42f0
YousefMahfouzz <youseftalal4@live.com>
2023-11-21 19:40:28 +0300

    Restful API

---
+++ src/main/java/com/restapi/restful/RestfulApplication.java
@@ -0,0 +1,16 @@
+package com.restapi.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class RestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApplication.class, args);
+
+
+	}
+}
+
